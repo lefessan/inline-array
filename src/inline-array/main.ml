d8dfc7c82bf4c92fa407cb480ffb16a90d4f065b
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2025-11-02 19:08:46 +0100

    inline_array_lib should only export InlineArray module

--- src/inline-array/main.ml
+++ src/inline-array/main.ml
@@ -10,5 +10,52 @@
 (*                                                                        *)
 (**************************************************************************)
 
+type t = {
+    x : int ;
+    mutable y : int ;
+    mutable z : int ;
+    xyz : string ;
+  }
 
-let () = Inline_array_lib.Main.main ()
+let make x y z =
+  { x ; y ; z ; xyz = Printf.sprintf "%d.%d.%d" x y z }
+
+let to_string a =
+  Printf.sprintf "%d.%d.%d.{%s}" a.x a.y a.z a.xyz
+
+let get t n =
+  Printf.eprintf "get[%d/%d] = %!" n (InlineArray.length t);
+  let s =
+  try
+    let a = InlineArray.get t n in
+    to_string a
+  with
+    exn ->
+    Printexc.to_string exn
+  in
+  Printf.eprintf "%s\n%!" s
+
+let print t =
+  let len = InlineArray.length t in
+  get t (-1) ;
+  get t len ;
+  InlineArray.iteri (fun i v ->
+      Printf.eprintf "get[%d/%d] = %s\n%!" i (InlineArray.length t)
+        (to_string v)
+    ) t;
+  ()
+
+let main () =
+  let a = make 0 0 0 in
+  let len = 10 in
+  let t = InlineArray.make len a in
+  print t;
+  let t = InlineArray.init len (fun i ->
+              make i 0 i) in
+  print t;
+  Gc.major ();
+  Gc.major ();
+  Gc.major ();
+  Gc.major ();
+  print t;
+  ()
